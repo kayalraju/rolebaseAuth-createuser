c1e5a5c1cfe44254e4fb075b551d701a12148e54
kayalraju <87748199+kayalraju@users.noreply.github.com>
2024-10-03 14:53:30 +0530

    change validation

--- app/helper/validation.js
+++ app/helper/validation.js
@@ -8,16 +8,16 @@ exports.adminRegistrationValidator = [
     gmail_lowercase: true,
     gmail_remove_dots: true,
   }),
-  check(
-    "password",
-    "Password must be greater than 6 character and contains at least one uppercase letter, one lowercase letter, one number and one special character"
-  ).isStrongPassword({
-    minLength: 6,
-    minUppercase: 1,
-    minLowercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  }),
+  // check(
+  //   "password",
+  //   "Password must be greater than 6 character and contains at least one uppercase letter, one lowercase letter, one number and one special character"
+  // ).isStrongPassword({
+  //   minLength: 6,
+  //   minUppercase: 1,
+  //   minLowercase: 1,
+  //   minNumbers: 1,
+  //   minSymbols: 1,
+  // }),
   check("profileImage")
     .custom((value, { req }) => {
       const allowedTypes = ["image/png", "image/jpg", "image/jpeg"];
@@ -72,21 +72,21 @@ exports.addUserValidator = [
     gmail_lowercase: true,
     gmail_remove_dots: true,
   }),
-  check(
-    "password",
-    "Password must be greater than 6 character and contains at least one uppercase letter, one lowercase letter, one number and one special character"
-  ).isStrongPassword({
-    minLength: 6,
-    minUppercase: 1,
-    minLowercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  }),
-  check("role")
-    .notEmpty()
-    .withMessage("Role is required")
-    .isIn(["manager", "hr", "team-leader", "employee", "office-boy"])
-    .withMessage("Invalid role selected"),
+  // check(
+  //   "password",
+  //   "Password must be greater than 6 character and contains at least one uppercase letter, one lowercase letter, one number and one special character"
+  // ).isStrongPassword({
+  //   minLength: 6,
+  //   minUppercase: 1,
+  //   minLowercase: 1,
+  //   minNumbers: 1,
+  //   minSymbols: 1,
+  // }),
+  // check("role")
+  //   .notEmpty()
+  //   .withMessage("Role is required")
+  //   .isIn(["manager", "hr", "team-leader", "employee", "office-boy"])
+  //   .withMessage("Invalid role selected"),
 
   check("profileImage")
     .custom((value, { req }) => {
