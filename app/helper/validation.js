23becaf76b85e8de68ab994c92fd012bc22c11f7
kayalraju <87748199+kayalraju@users.noreply.github.com>
2024-10-03 15:00:13 +0530

    dgdygjfgdj

--- app/helper/validation.js
+++ app/helper/validation.js
@@ -48,19 +48,19 @@ exports.loginValidator = [
     .isEmail()
     .withMessage("Please provide a valid email.")
     .normalizeEmail(),
-  check("password")
-    .not()
-    .isEmpty()
-    .isStrongPassword({
-      minLength: 6,
-      minUppercase: 1,
-      minLowercase: 1,
-      minNumbers: 1,
-      minSymbols: 1,
-    })
-    .withMessage(
-      "Password must be greater than 6 character and contains at least one uppercase letter, one lowercase letter, one number and one special character"
-    ),
+  // check("password")
+  //   .not()
+  //   .isEmpty()
+  //   .isStrongPassword({
+  //     minLength: 6,
+  //     minUppercase: 1,
+  //     minLowercase: 1,
+  //     minNumbers: 1,
+  //     minSymbols: 1,
+  //   })
+  //   .withMessage(
+  //     "Password must be greater than 6 character and contains at least one uppercase letter, one lowercase letter, one number and one special character"
+  //   ),
   ,
 ];
 
